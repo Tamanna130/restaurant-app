d4cb6ab50ad9dbf96aa04b95b808c5b420e9f326
Tamanna Shermin <tshermin201130@bscse.uiu.ac.bd>
2023-02-11 23:57:05 +0600

    Component tree

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './body/Body'
+import Header from './header/Header'
+import Footer from './footer/Footer'
+
+export default function MainComponent() {
+  return (
+    <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+    </div>
+  )
+}
